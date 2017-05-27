63c9a905fb5a62a63b89b210f1839c700d0bc03e
phonphey <phonphey@o2.pl>
2017-05-27 16:33:47 +0300

    Bug fix

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,20 @@
-
 extends KinematicBody2D
  
 var input_direction = 0
 var direction = 1
  
-var speed_x = 0
-var speed_y = 0
-
-const MAX_SPEED = 800
-const ACCELERATION = 2000
-const DECELERATION = 8000
-
-const JUMP_POWER = 400 
-const GRAVITY = 1600
+var speed = Vector2()
+var velocity = Vector2()
+var jump_count = 0
+ 
+const MAX_SPEED = 600
+const ACCELERATION = 1200
+const DECELERATION = 2000
+ 
+const JUMP_FORCE = 700
+const GRAVITY = 2000
 
 const MAX_JUMP_COUNT = 2
-
-var velocity = Vector2()
  
 func _ready():
     set_process(true)
@@ -24,37 +22,40 @@ func _ready():
  
  
 func _input(event):
-    var move_left = event.is_action_pressed("move_left")
-    var move_right = event.is_action_pressed("move_right")
-    var stop_moving = not (Input.is_action_pressed("move_right") or Input.is_action_pressed("move_left"))
-    var jump = event.is_action_pressed("jump")
-   
-    if move_left:
-        input_direction = -1
-    if move_right:
-        input_direction = 1
-    if stop_moving:
-        input_direction = 0
-    if jump:
-	    speed_y = -JUMP_POWER
-	
-		
-   
-    if move_left or move_right and input_direction:
-        if input_direction == -1 * direction:
-            speed_x /= 3
-            direction = input_direction
+    if jump_count < MAX_JUMP_COUNT and event.is_action_pressed("jump"):
+        speed.y = -JUMP_FORCE
+        jump_count += 1
  
  
 func _process(delta):
     if input_direction:
-        speed_x += ACCELERATION * delta
-    else:
-        speed_x -= DECELERATION * delta
-    speed_x = clamp(speed_x, 0, MAX_SPEED)
-
-    speed_y += GRAVITY * delta
+        direction = input_direction
    
-    velocity.x = speed_x * delta * direction
-    velocity.y = speed_y * delta
-    move(velocity)
+    if Input.is_action_pressed("move_left"):
+        input_direction = -1
+    elif Input.is_action_pressed("move_right"):
+        input_direction = 1
+    else:
+        input_direction = 0
+   
+    if input_direction == - direction:
+        speed.x /= 3
+    if input_direction:
+        speed.x += ACCELERATION * delta
+    else:
+        speed.x -= DECELERATION * delta
+    speed.x = clamp(speed.x, 0, MAX_SPEED)
+   
+    speed.y += GRAVITY * delta
+   
+    velocity = Vector2(speed.x * delta * direction, speed.y * delta)
+    
+    var movement_remainder = move(velocity)
+
+   
+    if is_colliding():
+        var normal = get_collision_normal()
+        var final_movement = normal.slide(movement_remainder)
+        speed = normal.slide(speed)
+        move(final_movement)
+        jump_count = 0
